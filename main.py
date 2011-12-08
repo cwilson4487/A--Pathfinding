e99bac792561309cc4884dfad058c3e7d64be5e9
Cole Schultz <schultzcole@gmail.com>
2011-12-07 21:46:46 -0600

    Woo! Gettin' started :)

---
+++ main.py
@@ -0,0 +1,3 @@
+## Written by Cole Schultz
+## A* pathfinding algorithm, plus some cool additions of my own (copied from Potado's ideas mostly, though. Except not his code. I haven't seen his code.)
+
